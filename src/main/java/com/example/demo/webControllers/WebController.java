15531873934b8741be4a86551441db0c3ab0e3a2
Cullen <90924902+cpw5800@users.noreply.github.com>
2022-12-01 12:52:40 -0500

    added text box to news page
    Signed-off-by: Cullen <90924902+cpw5800@users.noreply.github.com>

--- src/main/java/com/example/demo/webControllers/WebController.java
+++ src/main/java/com/example/demo/webControllers/WebController.java
@@ -23,19 +23,53 @@ public class WebController {
 
     @RequestMapping(value = {"/about.html"})
     public String about(){
-        log.debug("WebController.page2.()");
+        log.debug("WebController.about.()");
         return "about";
     }
 
     @RequestMapping(value = {"/submitAPost.html"})
     public String submit(){
-        log.debug("WebController.page2.()");
+        log.debug("WebController.submit.()");
         return "submitAPost";
     }
 
     @RequestMapping(value = {"/viralVideos.html"})
     public String viralVideos(){
-        log.debug("WebController.page2.()");
+        log.debug("WebController.viralVideos.()");
         return "viralVideos";
     }
+
+    @RequestMapping(value = {"/news.html"})
+    public String news(){
+        log.debug("WebController.news.()");
+        return "news";
+    }
+
+    @RequestMapping(value = {"/postVideo.html"})
+    public String postVideo(){
+        log.debug("WebController.postVideo.()");
+        return "postVideo";
+    }
+
+    @RequestMapping(value = {"/contact.html"})
+    public String contact(){
+        log.debug("WebController.contact.()");
+        return "contact";
+    }
+
+    @RequestMapping(value = {"/more_info.html"})
+    public String more_info(){
+        log.debug("WebController.more_info.()");
+        return "more_info";
+    }
+
 }
+
+/**
+ *
+  @RequestMapping(value = {"/.html"})
+      public String (){
+          log.debug("WebController..()");
+          return "";
+      }
+ */
